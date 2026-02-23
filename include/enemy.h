ca5e0b8bae54ceaa7405a4ee9b0a34c819bf91a5
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-02-23 23:45:49 +0200

    Created basic enemies that spawn on the pillars

---
+++ include/enemy.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdlib.h>
+#include "pillar.h"
+
+typedef struct Enemy {
+    float x;
+    float y;
+    float speed;
+    int width;
+    int height;
+    int id;
+    enum {MELEE = 0, RANGED = 1} type;
+} Enemy;
+
+typedef struct Enemies {
+    Enemy* items;
+    size_t count;
+    size_t capacity;
+} Enemies;
+
+Enemy initEnemy(float x, float y, float speed, int width, int height, int type, int id);
+void initEnemies(Enemies* enemies);
+void addEnemy(Enemies* enemies, Enemy enemy);
+void generateEnemies(Enemies* enemies, Pillars* pillars);
+void displayEnemies(Enemies* enemies);
+void freeEnemy(Enemies* enemies, int id);
---
+++ src/enemy.c
@@ -0,0 +1,77 @@
+#include "enemy.h"
+#include "raylib.h"
+#include "helpers.h"
+#include "pillar.h"
+#include <stdio.h>
+
+Enemy initEnemy(float x, float y, float speed, int width, int height, int type, int id) {
+    Enemy enemy;
+    enemy.x = x;
+    enemy.y = y;
+    enemy.speed = speed;
+    enemy.width = width;
+    enemy.height = height;
+    enemy.type = type;
+    enemy.id = id;
+    return enemy;
+}
+
+void initEnemies(Enemies* enemies) {
+    enemies->count = 0;
+    enemies->capacity = 10;
+    enemies->items = malloc(sizeof(Enemy) * enemies->capacity);
+}
+
+void addEnemy(Enemies* enemies, Enemy enemy) {
+    if (enemies->count >= enemies->capacity) {
+        enemies->capacity *= 2;
+        enemies->items = realloc(enemies->items, sizeof(Enemy) * enemies->capacity);
+    }
+    enemies->items[enemies->count++] = enemy;
+}
+
+void generateEnemies(Enemies* enemies, Pillars* pillars) {
+    for (size_t i = 0; i < pillars->count; i++) {
+
+        float x = (float)GetRandomValue(
+            (int)pillars->items[i].x,
+            (int)(pillars->items[i].x + pillars->items[i].width - 50)
+        );
+
+        float y = pillars->items[i].y - 50;
+
+        int type = GetRandomValue(0, 1);
+
+        float speed = (type == 0) ? 100.0f : 150.0f;
+
+        Enemy newEnemy = initEnemy(x, y, speed, 50, 50, type, i);
+
+        addEnemy(enemies, newEnemy);
+    }
+}
+
+void displayEnemies(Enemies* enemies) {
+    for (size_t i = 0; i < enemies->count; i++) {
+        Enemy* e = &enemies->items[i];
+        Color color = (e->type == 0) ? BLUE : GREEN;
+        DrawRectangle((int)e->x, (int)e->y, e->width, e->height, color);
+    }
+}
+
+void freeEnemy(Enemies *enemies, int id) {
+    for (size_t i = 0; i < enemies->count; i++) {
+        if (enemies->items[i].id == id) {
+            // Swap the enemy to be removed with the last enemy
+            enemies->items[i] = enemies->items[enemies->count - 1];
+            enemies->count--;
+            enemies->items = realloc(enemies->items, sizeof(Enemy) * enemies->count);
+            
+            // If realloc fails, handle memory allocation failure
+            if (enemies->items == NULL && enemies->count > 0) {
+                fprintf(stderr, "Memory allocation failed!\n");
+                exit(1);
+            }
+            break;
+        }
+    }
+}
